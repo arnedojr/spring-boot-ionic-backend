8de1d8a5424187db41e61762160fdeb08c38da52
Arnedo Jr <arnedojr@gmail.com>
2020-04-22 19:43:46 -0300

    Teste do Rest

--- src/main/java/com/arnedo/cursoionic/CursoionicApplication.java
+++ src/main/java/com/arnedo/cursoionic/CursoionicApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CursoionicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CursoionicApplication.class, args);
+		SpringApplication.run(CursoionicApplication.class, args); 
 	}
 
 }
