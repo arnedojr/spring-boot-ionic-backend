d418a80745362fc4ec15fd4dc0e67fbce96f3199
Arnedo Jr <arnedojr@gmail.com>
2020-05-04 22:35:53 -0300

    Criando operação de instanciação

--- src/main/java/com/arnedo/cursoionic/CursoionicApplication.java
+++ src/main/java/com/arnedo/cursoionic/CursoionicApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.arnedo.cursoionic.domain.Categoria;
+import com.arnedo.cursoionic.domain.Produto;
 import com.arnedo.cursoionic.repositories.CategoriaRepository;
+
 
 @SpringBootApplication
 public class CursoionicApplication implements CommandLineRunner {
@@ -24,6 +26,13 @@ public class CursoionicApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		Categoria cat1 = new Categoria(null, "Informática X");
 		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 800.00);
+		Produto p3 = new Produto(null, "Mouse", 80.00);
+		
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
