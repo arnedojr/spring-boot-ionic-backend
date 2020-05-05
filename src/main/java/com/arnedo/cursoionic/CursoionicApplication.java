620531514b3c1714acc58c7d236e4a8a0bdccce1
Arnedo Jr <arnedojr@gmail.com>
2020-05-05 12:18:48 -0300

    Estado e Cidade

--- src/main/java/com/arnedo/cursoionic/CursoionicApplication.java
+++ src/main/java/com/arnedo/cursoionic/CursoionicApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.arnedo.cursoionic.domain.Categoria;
+import com.arnedo.cursoionic.domain.Cidade;
+import com.arnedo.cursoionic.domain.Estado;
 import com.arnedo.cursoionic.domain.Produto;
 import com.arnedo.cursoionic.repositories.CategoriaRepository;
+import com.arnedo.cursoionic.repositories.CidadeRepository;
+import com.arnedo.cursoionic.repositories.EstadoRepository;
 import com.arnedo.cursoionic.repositories.ProdutoRepository;
 
 
@@ -21,6 +25,12 @@ public class CursoionicApplication implements CommandLineRunner {
 	
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursoionicApplication.class, args); 
@@ -44,6 +54,21 @@ public class CursoionicApplication implements CommandLineRunner {
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+		
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "São Paulo");
+		
+		Cidade c1 = new Cidade(null, "Uberlândia", est1);
+		Cidade c2 = new Cidade(null, "São Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+		
+		est1.setCidades(Arrays.asList(c1));
+		est2.setCidades(Arrays.asList(c2, c3));
+		
+		// o estado pode ter varias cidades ou nenhuma... deve ser salvo primeiro
+		// a cidade é dependente de um Estado criado, logo deverá ser salva depois 
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 	}
 
 }
