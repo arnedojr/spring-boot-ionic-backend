d200d5103e25ae85d062f616d4b9b52c1d0908ad
Arnedo Jr <arnedojr@gmail.com>
2020-05-04 21:07:17 -0300

    Criando repository e service para Categoria

--- src/main/java/com/arnedo/cursoionic/resources/CategoriaResource.java
+++ src/main/java/com/arnedo/cursoionic/resources/CategoriaResource.java
@@ -3,15 +3,22 @@ package com.arnedo.cursoionic.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.arnedo.cursoionic.domain.Categoria;
+import com.arnedo.cursoionic.services.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResource {
+	
+	@Autowired
+	private CategoriaService service;
 	
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Categoria> listar() {
@@ -24,5 +31,14 @@ public class CategoriaResource {
 		categorias.add(cat2);
 		
 		return categorias;
+	}
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		// o ? significa que pode retornar qualquer coisa.
+		
+		Categoria obj = service.buscar(id);
+		
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/arnedo/cursoionic/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.arnedo.cursoionic.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.arnedo.cursoionic.domain.Categoria;
+import com.arnedo.cursoionic.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+}
